c550b46da0e9ca2e9f6a88976e500d494d12f886
nramac <nithyabrc@gmail.com>
2015-04-22 17:06:18 +0530

    Updated IAOPageWaits - to include 2 methods - setEWait_InvisibilityOfElement;  setEWait_PresenceOfElement
    Change-Id: I8bf18fc337cdf13263423dfde716814034e7557a

--- src/org/vukfactory/iamone/ui/IAOPageWaits.java
+++ src/org/vukfactory/iamone/ui/IAOPageWaits.java
@@ -50,5 +50,63 @@ public class IAOPageWaits {
 		}
 		
 	}
+	
+	public void setEWait_InvisibilityOfElement(WebDriver driver, String sDomType, String sUIReference, int iTimeSeconds)
+	{
+		wait = new WebDriverWait(driver, iTimeSeconds);
+		if(sDomType.equals("linkText"))
+		{
+			wait.until(ExpectedConditions.invisibilityOfElementLocated(By.linkText(sUIReference)));
+		}
+		else if(sDomType.equals("id"))
+		{
+			wait.until(ExpectedConditions.invisibilityOfElementLocated(By.id(sUIReference)));
+		}
+		else if(sDomType.equals("className"))
+		{
+			wait.until(ExpectedConditions.invisibilityOfElementLocated(By.className(sUIReference)));
+		}
+		else if(sDomType.equals("name"))
+		{
+			wait.until(ExpectedConditions.invisibilityOfElementLocated(By.name(sUIReference)));
+		}
+		else if(sDomType.equals("xpath"))
+		{
+			wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath(sUIReference)));
+		}
+		else if(sDomType.equals("cssSelector"))
+		{
+			wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(sUIReference)));
+		}
+	}
+	
+	public void setEWait_PresenceOfElement(WebDriver driver, String sDomType, String sUIReference, int iTimeSeconds)
+	{
+		wait = new WebDriverWait(driver, iTimeSeconds);
+		if(sDomType.equals("linkText"))
+		{
+			wait.until(ExpectedConditions.presenceOfElementLocated(By.linkText(sUIReference)));
+		}
+		else if(sDomType.equals("id"))
+		{
+			wait.until(ExpectedConditions.presenceOfElementLocated(By.id(sUIReference)));
+		}
+		else if(sDomType.equals("className"))
+		{
+			wait.until(ExpectedConditions.presenceOfElementLocated(By.className(sUIReference)));
+		}
+		else if(sDomType.equals("name"))
+		{
+			wait.until(ExpectedConditions.presenceOfElementLocated(By.name(sUIReference)));
+		}
+		else if(sDomType.equals("xpath"))
+		{
+			wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(sUIReference)));
+		}
+		else if(sDomType.equals("cssSelector"))
+		{
+			wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(sUIReference)));
+		}
+	}
 
 }
